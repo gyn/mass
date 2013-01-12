8246f69844f58a19ce9b7a9f0c0c3243eaa305a3
Gao Ya'nan <abutter.gao@gmail.com>
2013-01-12 13:11:02 +0800

    Remove useless code

--- bitswap/bitswap.c
+++ bitswap/bitswap.c
@@ -1,49 +1,22 @@
-/***********************license start***************
- * Copyright (c) 2003-2010  Cavium Inc. (support@cavium.com). All rights 
- * reserved.
- *
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
-
- *   * Neither the name of Cavium Inc. nor the names of
- *     its contributors may be used to endorse or promote products
- *     derived from this software without specific prior written
- *     permission.  
-
- * This Software, including technical data, may be subject to U.S. export  control
- * laws, including the U.S. Export Administration Act and its  associated
- * regulations, and may be subject to export or import  regulations in other
- * countries. 
-
- * TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS" 
- * AND WITH ALL FAULTS AND CAVIUM INC. MAKES NO PROMISES, REPRESENTATIONS OR
- * WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT TO
- * THE SOFTWARE, INCLUDING ITS CONDITION, ITS CONFORMITY TO ANY REPRESENTATION OR
- * DESCRIPTION, OR THE EXISTENCE OF ANY LATENT OR PATENT DEFECTS, AND CAVIUM
- * SPECIFICALLY DISCLAIMS ALL IMPLIED (IF ANY) WARRANTIES OF TITLE,
- * MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR PURPOSE, LACK OF
- * VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET POSSESSION OR
- * CORRESPONDENCE TO DESCRIPTION. THE ENTIRE  RISK ARISING OUT OF USE OR
- * PERFORMANCE OF THE SOFTWARE LIES WITH YOU.
- ***********************license end**************************************/
+#include <stdio.h>
+#include <stdint.h>
 
 
-/*
- * File version info: $Id: hello.c 70030 2012-02-16 04:23:43Z cchavva $
- */
- 
-#include "cvmx.h"
+#if defined(CONFIG_CPU_CYCLE)
+#define CYCLE_DECL							\
+	uint64_t cycle_point;					\
+	uint32_t cycle_delta;
 
+#define CYCLE_START							\
+	cycle_point = cvmx_get_cycle();
+
+#define CYCLE_END							\
+	cycle_delta = cvmx_get_cycle() - cycle_point;
+#else
+#define CYCLE_DECL
+#define CYCLE_START
+#define CYCLE_END
+#endif
 
 uint64_t bitswap64(uint64_t value)
 {
@@ -88,20 +61,19 @@ int main(void)
 	uint32_t v32;
 	uint64_t v64;
 
-	uint64_t cycle_point;
-	uint64_t cycle_delta;
+	CYCLE_DECL
 
-	cycle_point = cvmx_get_cycle();
+	CYCLE_START
 	v32 = bitswap32(1);
-	cycle_delta = cvmx_get_cycle() - cycle_point;
+	CYCLE_END
 
-	printf("%s %d : %x %lx\n", __FUNCTION__, __LINE__, v32, cycle_delta);
+	printf("%s %d : %x\n", __FUNCTION__, __LINE__, v32);
 
-	cycle_point = cvmx_get_cycle();
+	CYCLE_START
 	v64 = bitswap64(1);
-	cycle_delta = cvmx_get_cycle() - cycle_point;
+	CYCLE_END
 
-	printf("%s %d : %lx %lx\n", __FUNCTION__, __LINE__, v64, cycle_delta);
+	printf("%s %d : %lx\n", __FUNCTION__, __LINE__, v64);
 
 	return 0;
 }
