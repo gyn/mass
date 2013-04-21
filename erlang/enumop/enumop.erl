1ae770ff20146a687b7423331117809c10aacd45
Gao Ya'nan <abutter.gao@gmail.com>
2013-04-21 16:26:54 +0800

    Add some comments for enumop

--- erlang/enumop/enumop.erl
+++ erlang/enumop/enumop.erl
@@ -1,28 +1,23 @@
-%%
-%%
-%%
--module (enumop).
+%%% ======================================================================
+%%% @author Gao Ya'nan <abutter.gao@gmail.com>
+%%% @doc Functions for enumlating operations between 4 number to get the
+%%%      given result 
+%%% @copyright 2013 Gao Ya'nan
+%%% @version 0.1
+%%% ======================================================================
 
-%%
-%%
-%%
--export ([guess_op/5]).
+-module(enumop).
 
-%%
-%%
-%%
--compile([debug_info]).
-
+%% API
+-export([guess_op/5]).
 
 guess_op(A, B, C, D, Result) ->
 	List = [[Op, Nr] || Op <- enum_op(), Nr <- enum_nr(A, B, C, D)],
-	Filter = make_filter(Result),
-	lists:filter(Filter, lists:map(fun eval_node/1, List)).
+	lists:filter(make_filter(Result), lists:map(fun eval_node/1, List)).
 
-
-%
-%
-%
+%%
+%% Put 4 numbers to its position.
+%%
 enum_nr(A, B, C, D) ->
 	List = [A, B, C, D],
 	[{W, X, Y, Z} || W <- List,
@@ -30,31 +25,31 @@ enum_nr(A, B, C, D) ->
 					 Y <- List -- [W, X],
 					 Z <- List -- [W, X, Y]].
 
-%
-%
-%
+%%
+%% Select 3 from 6 operators.
+%%
 enum_op() ->
 	List = [iadd, isub, imul, idiv, rsub, rdiv],
 	[{Opa, Opb, Opc} || Opa <- List, Opb <- List, Opc <- List].
 
-
+%%
+%% There are two different forms : Opc(Opa,Opb) and Opc(Opb(Opa)).
+%%
 eval_node(Node) ->
-	try 
+	try
 		[{Opa, Opb, Opc}, {W, X, Y, Z}] = Node,
 		Ra = eval(Opc, eval(Opb, eval(Opa, W, X), Y), Z),
 		Rb = eval(Opc, eval(Opa, W, X), eval(Opb, Y, Z)),
 		[{Ra, Rb} | Node]
 	catch
-		%
-		%
-		%
+		% Do not panic when meeting error:badarith
 		error:badarith ->
 			[{nil, nil} | Node]
 	end.
 
-%
-%
-%
+%%
+%% Work out the value by opcode and nubmers.
+%%
 eval(iadd, X, Y) -> X + Y;
 eval(isub, X, Y) -> X - Y;
 eval(rsub, X, Y) -> Y - X;
@@ -62,9 +57,9 @@ eval(imul, X, Y) -> X * Y;
 eval(idiv, X, Y) -> X / Y;
 eval(rdiv, X, Y) -> Y / X.
 
-%
-%
-%
+%%
+%% Make a filter for the right value.
+%%
 make_filter(Value) ->
 	fun ([{Ra, Rb}, _, _])->
 		(is_integer(Ra) andalso (Ra == Value)) orelse
