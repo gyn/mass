86465294a2dfe8699b5a049483af37a7832078a0
Gao Ya'nan <abutter.gao@gmail.com>
2013-04-21 17:32:30 +0800

    Rework raise function with tail recursion

--- erlang/e4e/powers.erl
+++ erlang/e4e/powers.erl
@@ -11,12 +11,14 @@
 -export ([raise/2]).
 
 %% -----------------------------------------------------------------------
-%% @doc 
+%% @doc Tail recursion with an accumulator
 %% -----------------------------------------------------------------------
 
 -spec(raise(number(),number()) -> number()).
 
-raise(_, 0) -> 1;
-raise(M, 1) -> M;
-raise(M, N) when N > 0 -> M * raise(M, N - 1);
-raise(M, N) when N < 0 -> 1.0 / raise(M, -N).
+raise(M, N) when N == 0 -> M;
+raise(M, N) when N > 0 -> raise(M, N, 1);
+raise(M, N) when N < 0 -> 1.0 / raise(M, -N, 1).
+
+raise(M, 1, Acc) -> M * Acc;
+raise(M, N, Acc) -> raise(M, N-1, Acc*M).
